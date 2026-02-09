2acabf04b34e532d89b01afcfdb67959a5254b15
Sannido <dimabutnik71@gmail.com>
2026-02-09 18:49:40 +0200

    Rename ExceptionHandler directory and namespace to plural form
    Renamed the `ExceptionHandler` directory to `ExceptionHandlers` in `Core.Api` and updated the namespaces in `ExceptionHandler.cs` and `UnknownStatusCodeException.cs` to match.

--- backend/DiplomaAwardingSystem/src/Core/Core.Api/ExceptionHandlers/ExceptionHandler.cs
+++ backend/DiplomaAwardingSystem/src/Core/Core.Api/ExceptionHandlers/ExceptionHandler.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace Core.Api.ExceptionHandler;
+namespace Core.Api.ExceptionHandlers;
 
 public sealed class ExceptionHandler(
     IProblemDetailsService problemDetailsService,
--- backend/DiplomaAwardingSystem/src/Core/Core.Api/ExceptionHandlers/UnknownStatusCodeException.cs
+++ backend/DiplomaAwardingSystem/src/Core/Core.Api/ExceptionHandlers/UnknownStatusCodeException.cs
@@ -1,6 +1,6 @@
 using System.Net;
 
-namespace Core.Api.ExceptionHandler;
+namespace Core.Api.ExceptionHandlers;
 
 public sealed class UnknownStatusCodeException(string endpointGroupName, string endpointName, HttpStatusCode statusCode)
     : Exception($"Endpoint group: {endpointGroupName} -> Endpoint: {endpointName} -> Unknown status code: {statusCode}");
